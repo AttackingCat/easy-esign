f64e848eb9b65d474801fa2a082ab6648e0d2fad
阿南 <626058643@qq.com>
2025-11-05 11:18:01 +0800

    添加获取填写合同模板页面的接口
    * 添加获取填写合同模板页面的接口及相关请求和响应类

--- easy-esign-api/src/main/java/io/github/easy/esign/DocTemplateSrv.java
+++ easy-esign-api/src/main/java/io/github/easy/esign/DocTemplateSrv.java
@@ -3,7 +3,9 @@ package io.github.easy.esign;
 import io.github.easy.esign.core.AbstractSrv;
 import io.github.easy.esign.struct.ESignResp;
 import io.github.easy.esign.struct.doc.req.DocTemplateEditUrlReq;
+import io.github.easy.esign.struct.doc.req.DocTemplateFillUrlReq;
 import io.github.easy.esign.struct.doc.resp.DocTemplateEditUrlResp;
+import io.github.easy.esign.struct.doc.resp.DocTemplateFillUrlResp;
 import io.github.easy.esign.struct.doc.resp.DocTemplateResp;
 import io.github.easy.esign.struct.doc.resp.DocTemplatesResp;
 
@@ -38,6 +40,14 @@ public class DocTemplateSrv extends AbstractSrv {
     public ESignResp<DocTemplateEditUrlResp> editUrl(DocTemplateEditUrlReq request) {
         String path = "/v3/doc-templates/" + request.getDocTemplateId() + "/doc-template-edit-url";
         return execute().post(path, request, DocTemplateEditUrlResp.class);
+    }
+
+    /**
+     * 获取填写合同模板页面
+     */
+    public ESignResp<DocTemplateFillUrlResp> fillUrl(DocTemplateFillUrlReq request) {
+        String path = "/v3/doc-templates/doc-template-fill-url";
+        return execute().post(path, request, DocTemplateFillUrlResp.class);
     }
 
 }
